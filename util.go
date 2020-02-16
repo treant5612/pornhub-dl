1d1d68bf84091f59828ecea0876a337d40c6ee76
treant <a1213467@gmail.com>
2020-02-17 01:49:39 +0800

    bugfix

--- util.go
+++ util.go
@@ -13,6 +13,5 @@ func httpGet(url string) (response *http.Response, err error) {
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
 
-	return client.Do(req)
+	return http.DefaultClient.Do(req)
 }
-
